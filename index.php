b7880fcad72acd98a482633beae2239bcf9fa21e
paulo-esco <116454139+paulo-esco@users.noreply.github.com>
2024-01-10 05:19:25 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, '213.171.15.88:3000?url=steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M4392756089663601682A33408863933D4755923023057005973');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+
+$headers = array();
+$headers[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7';
+$headers[] = 'Accept-Language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7';
+$headers[] = 'Cache-Control: max-age=0';
+$headers[] = 'Connection: keep-alive';
+$headers[] = 'Upgrade-Insecure-Requests: 1';
+$headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Errno:' . curl_errno($ch)."<br>";
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
+
+echo $result;
+
+?>
